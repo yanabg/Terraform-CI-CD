39eb7976b5264c4878551838c2c0afdd22281c6b
yana <yanabgeorgieva@gmail.com>
2024-12-09 23:07:03 +0200

    fixed container name

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "StorageRG"
     storage_account_name = "taskboardstorageybg"
-    container_name       = "taskboardstorageybg"
+    container_name       = "taskboardstorageybgcontainer"
     key                  = "terraform.tfstate"
   }
 }
